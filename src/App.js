9b8198e983fd43b43f7bef177a68d005bf471126
AldoIrvine <aldoirvine@gmail.com>
2021-03-05 23:34:35 +1100

    test

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello World 2.0</h1>
+        <h1>Hello World 3.0</h1>
       </header>
     </div>
   );
