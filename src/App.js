65a0d53bca36dc4f33460046e5e5e19e4f53b459
AldoIrvine <aldoirvine@gmail.com>
2021-03-05 23:44:34 +1100

    Fixed icon error

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Hello World</h1>
+        <h1>More to come :&#41;</h1>
       </header>
     </div>
   );
