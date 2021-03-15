90c7e9b73266a7a078734c236313531f1162e95b
AldoIrvine <aldoirvine@gmail.com>
2021-03-15 16:40:48 +1100

    added bg

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,13 @@
-import React from "react" ;
+import React from "react";
+import image from "../images/matcha.jpg";
 
 export default function Home() {
-    return <h1>Home Pages!</h1>
+  return (
+    <main>
+      <img src={image} alt="Matcha" className="absolute object-cover w-full" />
+      <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
+        <h1>Hello World. I'm Aldo</h1>
+      </section>
+    </main>
+  );
 }
